4aa86dca209bb7e62827737db561b86bdef9f3d0
ljj <1093261738@qq.com>
2024-01-14 22:11:27 +0800

    打开相册和相机功能

--- XDPapp/src/main/java/com/spread/xdpartner/main/MainActivity.kt
+++ XDPapp/src/main/java/com/spread/xdpartner/main/MainActivity.kt
@@ -1,17 +1,24 @@
 package com.spread.xdpartner.main
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.example.xdpartner.databinding.ActivityMainBinding
+import com.spread.xdpartner.add.AddActivity
 import com.spread.xdpartner.base.BaseViewBindingActivity
 import com.spread.xdpartner.main.fragment.FriendFragment
 import com.spread.xdpartner.main.fragment.MeFragment
 import com.spread.xdpartner.main.fragment.MessageFragment
 import com.spread.xdpartner.main.fragment.SearchFragment
 import com.spread.xdpartner.test.TestLogger.log
-
+const val index_home = 0
+const val index_message = 1
+const val index_friend = 2
+const val index_me = 3
+const val index_add = 4
+const val fragment_size = 4
 class MainActivity : BaseViewBindingActivity<ActivityMainBinding>() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,38 +29,46 @@ class MainActivity : BaseViewBindingActivity<ActivityMainBinding>() {
         binding.indexVpFragmentListTop.adapter = object : FragmentStateAdapter(this) {
             override fun createFragment(position: Int): Fragment {
                 return when (position) {
-                    0 -> SearchFragment()
-                    1 -> MessageFragment()
-                    2 -> FriendFragment()
-                    3 -> MeFragment()
+                    index_home -> SearchFragment()
+                    index_message -> MessageFragment()
+                    index_friend -> FriendFragment()
+                    index_me -> MeFragment()
                     else -> SearchFragment()
                 }
             }
 
-            override fun getItemCount() = 4
+            override fun getItemCount() = fragment_size
         }
-        binding.indexVpFragmentListTop.currentItem = 0
+        binding.indexVpFragmentListTop.currentItem = index_home
         binding.indexBottomBarHome.isSelected = true
-        binding.indexBottomBarHome.setOnClickListener(TabOnClickListener(0))
-        binding.indexBottomBarMessage.setOnClickListener(TabOnClickListener(1))
-        binding.indexBottomBarFriend.setOnClickListener(TabOnClickListener(2))
-        binding.indexBottomMe.setOnClickListener(TabOnClickListener(3))
+        binding.indexBottomBarHome.setOnClickListener(TabOnClickListener(index_home))
+        binding.indexBottomBarMessage.setOnClickListener(TabOnClickListener(index_message))
+        binding.indexBottomBarFriend.setOnClickListener(TabOnClickListener(index_friend))
+        binding.indexBottomMe.setOnClickListener(TabOnClickListener(index_me))
+        binding.indexBottomBarScan.setOnClickListener(TabOnClickListener(index_add))
     }
 
     override fun getViewBinding(): ActivityMainBinding {
         return ActivityMainBinding.inflate(layoutInflater)
     }
 
+    private fun startAddActivity(){
+        startActivity(Intent(this,AddActivity::class.java))
+    }
     inner class TabOnClickListener(var index : Int) : View.OnClickListener {
         override fun onClick(p0: View) {
             log("onclick $index")
-            refreach()
-            p0.isSelected = true
-            binding.indexVpFragmentListTop.setCurrentItem(index)
+            refresh()
+            if(index == index_add){
+                startAddActivity()
+            }else{
+                p0.isSelected = true
+                binding.indexVpFragmentListTop.setCurrentItem(index)
+            }
 
         }
 
-        private fun refreach() {
+        private fun refresh() {
             binding.indexBottomBarHome.isSelected = false
             binding.indexBottomBarFriend.isSelected = false
             binding.indexBottomBarMessage.isSelected = false
