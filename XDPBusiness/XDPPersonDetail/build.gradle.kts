ccb7245b5a9f4c148b95867dbad3f6c3c8cc58ea
ljj <1093261738@qq.com>
2024-04-03 22:41:31 +0800

    增加个人模块

---
+++ XDPBusiness/XDPPersonDetail/build.gradle.kts
@@ -0,0 +1,44 @@
+plugins {
+    id("com.android.library")
+    id("kotlin-kapt")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.spread.xdppersondetail"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 29
+        consumerProguardFiles("consumer-rules.pro")
+        kapt {
+            arguments {
+                arg("AROUTER_MODULE_NAME", project.name)
+            }
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    buildFeatures {
+        viewBinding = true
+        dataBinding = true
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+}
+dependencies {
+    api(project(":XDPLib"))
+    api(project(":XDPNetwork"))
+    kapt("com.alibaba:arouter-compiler:1.5.2")
+}
